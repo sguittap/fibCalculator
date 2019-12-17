49c91ee6c2525c481741bb16f110967d7c4f08ff
Shane Guittap <shaneguittap@Shanes-MacBook-Pro.local>
2019-12-17 13:40:19 -0500

    trying to fix 502 status error

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, {Fragment} from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Fib from './Pages/Fib';
+import OtherPage from './Pages/OtherPage';
+
+
+function App() {
+  return (
+    <Router>
+      <Fragment>
+        <header>
+          <h1>hi im your react app</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </Fragment>
+    </Router>
+  );
+}
+
+export default App;
